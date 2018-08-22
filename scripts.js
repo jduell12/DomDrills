47d54acbb5f9cd8e8de79d3144fa285e9d0ec3af
Jessica Duell <duellrdh@gmail.com>
2018-08-21 23:12:15 -0400

    created an h2 element

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function(){
     let div1 = document.createElement('div');
     div1.className = "header-container";
 
     let h1 = document.createElement('h1');
     let h1Text = document.createTextNode("This is an h1");
     h1.appendChild(h1Text);
-    div.appendChild(h1);
+
+    let h2 = document.createElement('h2');
+    let h2Text = document.createTextNode("This is an h2");
+    h2.appendChild(h2Text);
+
+    div1.appendChild(h1);
+    div1.appendChild(h2);
+    document.body.appendChild(div1);
+
+   
 });
