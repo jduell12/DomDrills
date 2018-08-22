ba22c7dfb8dcafaee618f783d0a692620d7554bc
Jessica Duell <duellrdh@gmail.com>
2018-08-21 23:01:06 -0400

    added a div and a h1 to DOM

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    let div1 = document.createElement('div');
+    div1.className = "header-container";
+
+    let h1 = document.createElement('h1');
+    let h1Text = document.createTextNode("This is an h1");
+    h1.appendChild(h1Text);
+    div.appendChild(h1);
+});
